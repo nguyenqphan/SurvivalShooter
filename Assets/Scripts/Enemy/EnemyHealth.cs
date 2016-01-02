9a9c05eed6d6f8ba5c2a82a5638c5d8f9539a2ad
Nguyen Phan <nqphan212@gmail.com>
2016-01-01 21:07:15 -0500

    Display scores

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+	using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
@@ -74,7 +74,7 @@ public class EnemyHealth : MonoBehaviour
         GetComponent <NavMeshAgent> ().enabled = false;
         GetComponent <Rigidbody> ().isKinematic = true;
         isSinking = true;
-        //ScoreManager.score += scoreValue;
+        ScoreManager.score += scoreValue;
         Destroy (gameObject, 2f);
     }
 }
